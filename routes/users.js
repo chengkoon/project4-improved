999d92d3edb7c174365c32e1098855c957254abf
chengkoon <chengkoon@gmail.com>
2017-06-14 12:58:38 +0800

    removes references to config/database file that had been deleted

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const config = require('../config/database');
 const User = require('../models/user');
 
 // Register
@@ -37,7 +36,7 @@ router.post('/authenticate', (req, res, next) => {
     User.comparePassword(password, user.password, (err, isMatch) => {
       if(err) throw err;
       if(isMatch){
-        const token = jwt.sign(user, config.secret, {
+        const token = jwt.sign(user, process.env.secret, {
           expiresIn: 604800 // 1 week
         });
 
