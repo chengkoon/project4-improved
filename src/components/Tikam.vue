0fa294117ddf172c577dff5f7f5af0e3734c4b4a
chengkoon <chengkoon@gmail.com>
2017-06-25 13:55:08 +0800

    adds search bar and search terms are highlighted

--- src/components/Tikam.vue
+++ src/components/Tikam.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="tikam"> <!-- list of items -->
-    <button type="button" @click="getItems">getItems</button>
+    <div class="columns">
+      <div class="field column is-4">
+        <p class="control has-icons-left">
+          <input class="input" type="text" v-model="searchTerm" placeholder="Do an instant search here">
+          <span class="icon is-small is-left">
+            <i class="fa fa-search"></i>
+          </span>
+        </p>
+      </div>
+    </div>
     <div class="columns is-10">
-      <div class="card column is-one-quarter" v-for="item in items">
+      <div class="card column is-one-quarter" v-for="item in filteredList">
         <div class="card-image">
           <figure class="image is-4by3">
             <img :src="item.imgURL" alt="Image">
@@ -16,13 +25,15 @@
               </figure>
             </div>
             <div class="media-content">
-              <a :href="item.productURL"><p class="title is-4">{{item.name}}</p></a>
+              <!-- <a :href="item.productURL"><p class="title is-4">{{item.name}}</p></a> -->
+              <a :href="item.productURL"><p class="title is-4"><span v-html="highlight(item.name)"></span></p></a>
               <p class="subtitle is-6">@johnsmith</p>
+              <!-- <span v-html="rawHTML"></span> -->
             </div>
           </div>
 
           <div class="content">
-            {{item.description}}
+            <span v-html="highlight(item.description)"></span>
           </div>
         </div>
         <footer class="card-footer">
@@ -60,23 +71,59 @@ export default {
     return {
       myheight: '',
       user: 'hehe',
-      items: {}
+      searchTerm: '',
+      items: [],
+      rawHTML: '<span class=\'highlight\'>hahaha</span>',
+      test: 'hahahehe'
     }
   },
-  methods: {
-    getItems () {
-      services.getItems().then(items => {
-        this.items = items
-        console.log('this.items are...', this.items)
+  computed: {
+    filteredList () {
+      return this.items.filter((item) => {
+        return item.name.includes(this.searchTerm) || item.description.includes(this.searchTerm)
       })
-    },
+    }
+  },
+  // filters: {
+  //   highlight (value) {
+  //     // if (!value) return ''
+  //     // // value = value.toString()
+  //     // console.log('capitalize value is...', typeof value)
+  //     // // return value.charAt(0).toUpperCase() + value.slice(1)
+  //     // return ('<span class=\'highlight\'>' + value + '</span>')
+  //     var iQuery = new RegExp(this.searchTerm, 'ig')
+  //     return value.toString().replace(iQuery, matchedTxt => {
+  //       return ('<span class=\'highlight\'>' + matchedTxt + '</span>')
+  //     })
+  //   }
+  // },
+  methods: {
     showItemDetails (itemId) {
       EventBus.$emit('item-details-modal', itemId)
+    },
+    highlight (text) {
+      if (this.searchTerm) {
+        let iQuery = new RegExp(this.searchTerm, 'ig')
+        text = text.toString().replace(iQuery, matchedTxt => {
+          return ('<span style=\'background-color: yellow\'>' + matchedTxt + '</span>')
+        })
+      }
+      console.log('text is now ', text)
+      return `<span>${text}</span>`
     },
     determineWinner (itemId) {
       console.log('DW client')
       axios.post('http://localhost:3000/item/determineWinner', { itemId: itemId })
     }
+  },
+  created () {
+    services.getItems().then(items => {
+      this.items = items
+      console.log('this.items is...', this.items)
+      for (let item of this.items) {
+        console.log('item is ', item)
+      }
+    })
   }
 }
 </script>
@@ -111,5 +158,9 @@ li {
 
 a {
   color: #42b983;
+}
+
+.highlight {
+  color: yellow;
 }
 </style>
